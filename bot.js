0032635854a2e2ca01e71cbe5824eeb61f118641
SalmanFa <48174332+SalmanFa@users.noreply.github.com>
2019-05-11 19:49:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on("message", (message) => {
         
         if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
         message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
-            let role = message.guild.roles.find("name", "Support");
+            let role = message.guild.roles.find("name", "Trial Support.");
             let role2 = message.guild.roles.find("name", "@everyone");
             c.overwritePermissions(role, {
                 SEND_MESSAGES: true,
